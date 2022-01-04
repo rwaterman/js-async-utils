034bd115bebcfcc40d80c5a3f42260e833e028d7
Rick Waterman <rick.waterman@jupiterone.com>
2022-01-04 15:26:24 -0800

    refactor: remove generic

--- src/helpers/delay.ts
+++ src/helpers/delay.ts
@@ -1,4 +1,4 @@
-export async function delay<T>(ms: number): Promise<void>  {
+export async function delay(ms: number): Promise<void>  {
   return new Promise((resolve) => setTimeout(() => {
     resolve();
   }, ms));
